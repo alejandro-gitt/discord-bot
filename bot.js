32a61217693f861affb9593ec3a82cc308aac64a
Alejandro Lares Pacheco <aleartpro@gmail.com>
2020-11-13 20:32:22 +0100

    Añadido: Barba clima (ciudad como parametro)

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 console.log("HENLO :))")
 
 const Discord = require('discord.js')
+const weather = require('weather-js')
+
 const client = new Discord.Client()
 client.login('Nzc2MTgxMzYxMTM1ODQ1NDM2.X6xJOQ.5hWd8cqUnEzKYn0_iiEemCpcDuM')
 client.on('ready',readyDiscord)
@@ -39,8 +41,23 @@ var lbda = "769290387927597056"
 
 client.on('message',handleMessage)
 function handleMessage(msg){
+
   if (msg.channel.id == generalId && msg.content == 'Who is daddy'){
     msg.reply('Pacheco 💖')
+  }
+
+  if (msg.channel.id == generalId && msg.content == 'Barba help'){
+    const embed = new Discord.MessageEmbed()
+    embed.setTitle('Guía de uso del bot Barba')
+    embed.addField('¿Qué es Barba?','Barba es un bot hecho por @aleky29#0989 el 12/11/2020, sin propósito concreto')
+    embed.addField('FUNCIONES:','A Continuación se listan las funciones, como llamarlas y para que sirven')
+    embed.addField('Squad members online (o offline):',' Lista los miembros humanos y online (o offline) del servidor')
+    embed.addField('Barba stats','Número de miembros online y offline')
+    embed.addField('Barba mata','Elige un miembro online al azar')
+    embed.addField('Barba mis mensajes','Te devuelve tus últimos cien mensajes del chat (DONT)')
+    embed.addField('Barba clima en (ciudad)','Obtiene el estado metorológico de la ciudad especificada')
+
+    msg.channel.send(embed)
   }
 
   if (msg.channel.id == generalId && msg.content == 'Squad members online'){
@@ -76,7 +93,7 @@ function handleMessage(msg){
     msg.channel.send(Embed)
   }
 
-  if (msg.channel.id == generalId && msg.content == 'Stats'){
+  if (msg.channel.id == generalId && msg.content == 'Barba stats'){
     // Get the Guild and store it under the variable "list"
     const list = client.guilds.cache.get(lbda);
 
@@ -101,7 +118,6 @@ function handleMessage(msg){
     onlinesnobots = onlines.filter(member => member.user.bot === false)
     //onlinesnobots.each(member => msg.reply(member.user.username))
     victimaKey = onlinesnobots.randomKey()
-    //respuesta =
     msg.reply('MUAJAJAJAJAJAJ. Adiós '+onlinesnobots.get(victimaKey).user.username)
 
 
@@ -126,27 +142,49 @@ function handleMessage(msg){
           return;
       }
   })
-}
+  }
+    if (msg.channel.id == generalId && msg.content.startsWith("Barba clima en")){
+      let words = msg.content.split(" ")
+      place = words[3]
+    weather.find({search: place, degreeType: 'C'},function(err,result){
+      if(err) console.log(err);
 
 
+      // console.log(JSON.stringify(result,null,2));
+      // msg.reply("INFUNCTION")
+      let snow = "❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️"
+      let sun = "☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️"
+      let cloudy = "☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️"
+      try{
+        let embed = new Discord.MessageEmbed()
+        embed.setTitle(`Weather - ${result[0].location.name}`)
+        embed.setColor("#ff2050")
+        if(result[0].current.skytext == "Sunny"){
+          embed.setDescription(sun)
+        }
+        else if(result[0].current.skytext == "Cloudy"){
+          embed.setDescription(cloudy)
+        }
+        else{
+          embed.setDescription("Que tengas un buen día :)")
+        }
+        embed.addField("Temperature", `${result[0].current.temperature} Celsius`, true)
+        embed.addField("Sky Text", result[0].current.skytext, true)
+        embed.addField("Humidity", result[0].current.humidity, true)
+        embed.addField("Wind Speed", result[0].current.windspeed, true)
+        embed.addField("Observation Time", result[0].current.observationtime, true)
+        embed.addField("Wind Display", result[0].current.winddisplay, true)
+        msg.channel.send(embed)
+      }catch(err){
+        msg.reply("Unable to get data of given location")
+      }
+
+    })
   }
 
+  if (msg.channel.id == generalId && msg.content == 'Barba texto ejemplo'){
+    let words = msg.content.split(" ")
+    msg.reply(words[2])
+  }
 
-  // async function userMessages(guildID, userID){
-  //   result = [];
-  //   client.guilds.cache.get(guildID).channels.cache.forEach(ch => {
-  //       if (ch.type === 'text'){
-  //           ch.messages.fetch({
-  //               limit: 100
-  //           }).then(messages => {
-  //               const msgs = messages.filter(m => m.author.id === userID)
-  //               msgs.forEach(m => {
-  //                 result.push(`${m.content} - ${m.channel.name}`)
-  //                   //console.log(`${m.content} - ${m.channel.name}`)
-  //               })
-  //           })
-  //       return result
-  //     } else {
-  //           return;
-  //       }
-  //   })
+}
